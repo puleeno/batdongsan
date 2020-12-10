589ee824c6ee0118a2afcac33390452b1a1e93c8
Puleeno Nguyen <puleeno@gmail.com>
2020-12-10 19:04:21 +0700

    Follow new structure of WordLand

--- includes/class-batdongsan-legal-status.php
+++ includes/class-batdongsan-legal-status.php
@@ -6,12 +6,6 @@ class Datdongsan_Legal_Status {
 
     public function __construct() {
         add_action('init', array($this, 'registerTaxonomies'));
-        add_filter(
-            'wordland_property_supported_json_fields',
-            array($this, 'appendLegalJsonToProperty'),
-            10,
-            2
-        );
         add_filter('wordland_builder_get_property', array($this, 'parseLegalData'), 10, 2);
     }
 
@@ -28,17 +22,6 @@ class Datdongsan_Legal_Status {
             'batdongsan_legal_args',
             $args
         ));
-    }
-
-    public function appendLegalJsonToProperty($data, $property) {
-        if (isset($property->legals)) {
-            $data['legals'] = $property->legals;
-        }
-        if (isset($property->legal)) {
-            $data['legal'] = $property->legal;
-        }
-
-        return $data;
     }
 
     public function parseLegalData($property, $builder) {
