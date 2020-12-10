67a22b44f954d250901d94ada18d3bb314889ea0
Puleeno Nguyen <puleeno@gmail.com>
2020-12-10 23:39:31 +0700

    Add default legal to property if it is not setted

--- includes/class-batdongsan.php
+++ includes/class-batdongsan.php
@@ -27,5 +27,17 @@ class Batdongsan {
 
 	public function init_hooks() {
 		register_activation_hook( BATDONGSAN_PLUGIN_FILE, array( Batdongsan_Install::class, 'active' ) );
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	public function init() {
+		add_action( 'save_post', array( $this, 'save_default_legal' ), 30 );
+	}
+
+	public function save_default_legal( $post_id ) {
+		$terms = wp_get_post_terms( $post_id, Datdongsan_Legal_Status::LEGAL_NAME );
+		if ( empty( $terms ) && ( $default_legal = get_option( 'default_property_legal' ) ) ) {
+			wp_set_post_terms( $post_id, array( intval( $default_legal ) ), Datdongsan_Legal_Status::LEGAL_NAME );
+		}
 	}
 }
