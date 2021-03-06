0dd46189308577ca2f0f203cfc900c3f2403568f
Puleeno Nguyen <puleeno@gmail.com>
2021-03-07 02:03:37 +0700

    Đăng ký thêm taxonomy hướng nhà cho BDS

--- includes/integrations/class-batdongsan-integration-spreadsheet-importer.php
+++ includes/integrations/class-batdongsan-integration-spreadsheet-importer.php
@@ -7,6 +7,8 @@ class Batdongsan_Integration_Spreadsheet_Importer {
 
 	public function create_batdongsan_import_data() {
 		add_filter( 'wordland_spreadsheet_importer_set_legal_status_data', array( $this, 'clone_value' ), 10, 3 );
+		add_filter( 'wordland_spreadsheet_importer_set_door_direction_data', array( $this, 'clone_value' ), 10, 3 );
+
 		add_action( 'wordland_spreadsheet_import_property', array( $this, 'save_batdongsan_data' ), 10, 2 );
 	}
 
@@ -32,6 +34,28 @@ class Batdongsan_Integration_Spreadsheet_Importer {
 					$property_id,
 					array( intval( $legal_term['term_id'] ) ),
 					Datdongsan_Legal_Status::LEGAL_NAME,
+					false
+				);
+			}
+		}
+
+		$door_direction = apply_filters(
+			'batdongsan_spreadsheet_importer_door_direction',
+			$property_data->door_direction,
+			$property_data
+		);
+
+		if ( $door_direction ) {
+			$direction_term = term_exists( $door_direction, Batdongsan_Front_Door_Direction::DOOR_DIRECTION_TAX );
+			if ( ! $direction_term ) {
+				$direction_term = wp_insert_term( $door_direction, Batdongsan_Front_Door_Direction::DOOR_DIRECTION_TAX );
+			}
+
+			if ( ! is_wp_error( $direction_term ) && $direction_term ) {
+				wp_set_post_terms(
+					$property_id,
+					array( intval( $direction_term['term_id'] ) ),
+					Batdongsan_Front_Door_Direction::DOOR_DIRECTION_TAX,
 					false
 				);
 			}
